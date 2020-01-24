804c23efe1ce790d5d4cf344e9984f7ed7676ddb
Terren Culberson <culbersonterren@yahoo.com>
2020-01-24 01:45:24 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,18 +1,1 @@
-class Project	
-  attr_reader :title, :backers	
 
-  def initialize(title)	
-    @title = title	
-    @backers = []
-  end	
-
-  def backers	
-    pbs = ProjectBacker.all.select {|pb| pb.project == self}	
-    pbs.map {|pb| pb.backer}	
-  end	
-
-  def add_backer(backer)	
-    ProjectBacker.new(self, backer)	
-  end	
-
-end
